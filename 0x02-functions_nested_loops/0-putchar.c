ed23b20689abe6a6a3bb6e294e28b997f14b3d57
Ahmedfelfel <Ghghmp.57@gmail.com>
2023-04-01 15:28:30 -0700

    add

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
-*main-print putchar
-*Return: always 0
-**/
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
 int main(void)
 {
 _putchar('_');
@@ -16,3 +18,4 @@ _putchar('r');
 _putchar('\n');
 return (0);
 }
+
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,26 @@
 #include "main.h"
+
 /**
-*main-print_alphabet_x10
-*Return: always 0
-**/
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
 void print_alphabet_x10(void)
 {
-char c, i;
-for (i=0;i<10;i++)
-  {
-   for (c='a';c<='z';c++)
-     {
-      _putchar(c);
-     }
-_putchar('\n');
-  }
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
+
